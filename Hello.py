1c80ff33b04a00bda23ccfbc29a76d8fac6af904
casperhva <145566192+casperhva@users.noreply.github.com>
2023-11-10 17:23:00 +0000

    V1

--- Hello.py
+++ Hello.py
@@ -1,51 +1,204 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
-
-LOGGER = get_logger(__name__)
-
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+import pandas as pd
+import plost
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import math
+import plotly.graph_objects as go
+import statsmodels.api as sm
+import cbsodata
+import ipywidgets as widgets
+from ipywidgets import interact
+import json
+import geopandas as gpd
+from folium.plugins import MarkerCluster
+from folium.features import Choropleth
+import folium
+from streamlit_folium import folium_static
+from folium import Choropleth
+from folium.plugins import HeatMap
+from branca.colormap import linear
+ 
+#inladen data
+calendar = pd.read_csv("calendar.csv")
+listing = pd.read_csv("listings.csv")
+#listingd = pd.read_csv("listings_details.csv")
+neighbourhood = pd.read_csv("neighbourhoods.csv")
+geo_ams = gpd.read_file('neighbourhoods.geojson')
+ 
+neighbourhoods = pd.merge(geo_ams, neighbourhood, on='neighbourhood', how='left')
+price = listing.groupby('neighbourhood').price.mean()
+ 
+#data cleaning
+df = calendar.sort_values('price', ascending = False)
+df_unique = df.drop_duplicates(subset='listing_id', keep='first').copy()
+df_unique_nan = df_unique.dropna()
+ 
+#prijs een cijfer maken
+df['price'] = df['price'].astype('str')
+df['price'] = df['price'].apply(lambda x:x.lstrip('$'))
+df['price'] = df['price'].apply(lambda x:x.replace(',',''))
+df['price'] = df['price'].astype('float')
+df_unique['price'] = df_unique['price'].astype('str')
+df_unique['price'] = df_unique['price'].apply(lambda x:x.lstrip('$'))
+df_unique['price'] = df_unique['price'].apply(lambda x:x.replace(',',''))
+df_unique['price'] = df_unique['price'].astype('float')
+#listingd['price'] = listingd['price'].astype('str')
+#listingd['price'] = listingd['price'].apply(lambda x:x.lstrip('$'))
+#listingd['price'] = listingd['price'].apply(lambda x:x.replace(',',''))
+#listingd['price'] = listingd['price'].astype('float')
+ 
+df = listing
+price = df.groupby('neighbourhood').price.mean()
+#price = price.reset_index()
+number_of_offers = df.groupby('neighbourhood').count()
+number_of_offers.rename(columns={'name': 'offers'}, inplace=True)
+neighbourhoods.set_index('neighbourhood', inplace=True)
+apartments = df[['latitude','longitude','room_type']]
+buurten = df.neighbourhood.unique()
+#kaart
+geo_ams["longitude"] = geo_ams.centroid.x
+geo_ams["latitude"] = geo_ams.centroid.y
+geo_ams.drop('neighbourhood_group', axis=1, inplace=True)
+neighbourhood_price = df.groupby('neighbourhood')['price'].mean()
+neighbourhood_price = pd.DataFrame({'neighbourhood':neighbourhood_price.index, 'price': neighbourhood_price.T.values})
+neighbourhood_price.sort_values('price', ascending=False, inplace=True)
+ 
+ 
+ 
+ 
+st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+ 
+st.sidebar.header('Select')
+ 
+ 
+st.sidebar.subheader('Choose your price')
+max_prijs = st.sidebar.slider('Max price', 0,800, value=400)
+plot_data = st.sidebar.multiselect('Select Neighboorhood', buurten, buurten)
+ 
+#kaart count neighbourhood
+df_sidebar = df.loc[(df.price<max_prijs)]
+ 
+neighbourhood_count = pd.DataFrame({
+    'neighbourhood': df_sidebar['neighbourhood'].value_counts().index,
+    'count': df_sidebar['neighbourhood'].value_counts().values
+})
+ 
+ 
+ 
+# Row A
+st.title('Dashboard')
+df_gem_prijs = df_sidebar.loc[df_sidebar.neighbourhood.isin(plot_data)]
+# Bereken het gemiddelde van de prijzen in de geselecteerde buurten en rond het af naar het dichtstbijzijnde hele getal
+gemiddelde_prijs = round(df_gem_prijs['price'].mean())
+df_total_listing = df_sidebar.loc[df_sidebar.neighbourhood.isin(plot_data)]
+total_listings = df_total_listing.id.nunique()
+ 
+# Row A
+st.markdown('### Summary')
+col1, col2, col3 = st.columns(3)
+col1.metric("Mean price", gemiddelde_prijs)
+col2.metric("Total listings", total_listings)
+ 
+# Row B
+ 
+Q1 = df_sidebar['price'].quantile(0.25)
+Q3 = df_sidebar['price'].quantile(0.75)
+IQR = Q3 - Q1
+filtered_df = df_sidebar[(df_sidebar['price'] >= Q1 - 1.5 * IQR) & (df_sidebar['price'] <= Q3 + 1.5 * IQR)]
+ 
+ 
+c1, c2 = st.columns((3,7))
+with c1:
+    st.markdown('### Heatmap')
+   
+ 
+# Plot de boxplot met Plotly Express zonder outliers
+    fig = px.box(filtered_df, y='price')
+ 
+# Update the title and x-axis label
+    fig.update_layout(
+        title_text='Price Boxplot',
+        xaxis_title='Price',
+        width=300,  # Adjust the width as needed
     )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-
-if __name__ == "__main__":
-    run()
+ 
+# Display the boxplot in Streamlit hallo
+    st.plotly_chart(fig)
+with c2:
+    st.markdown("### Map of the number of Airbnb's per neighbourhood ")
+    # Streamlit-app lay-out en functionaliteit
+    map_ams_count = folium.Map(location=[52.37, 4.89], zoom_start=11)
+    choropleth = folium.Choropleth(
+    geo_data=geo_ams,
+    data=neighbourhood_count,
+    columns=['neighbourhood', 'count'],
+    key_on='feature.properties.neighbourhood',
+    fill_color='YlOrRd',
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name='The number of properties',
+    reset=True,
+    highlight=True,  # Om highlighting toe te voegen
+    line_color='black',  # Kleur van de grenzen tussen de buurten
+    line_weight=1,  # Dikte van de lijnen tussen de buurten
+     # Doorzichtigheid van de lijnen tussen de buurten
+    name='Choropleth'
+    ).add_to(map_ams_count)
+ 
+# Voeg tooltip toe met de beschikbare velden
+    folium.GeoJsonTooltip(fields=['neighbourhood'], aliases=['Neighbourhood:']).add_to(choropleth.geojson)
+ 
+# Toon de kaart in Streamlit
+    folium_static(map_ams_count)
+ 
+     
+ 
+#row c
+c3, c4 = st.columns((5,5))
+with c3:
+ 
+    st.markdown('### Average price per neighbourhood')
+ 
+ 
+# Voeg Choropleth-laag toe aan de kaart met tooltip
+ 
+ 
+#barplot over average price of room
+ 
+# Creëer een figuur met matplotlib
+    fig, ax = plt.subplots(figsize=(14, 10), dpi=80)
+ 
+# Gebruik seaborn voor de staafplot
+    barplot_price_room1 = price.reset_index()
+    barplot_price_room = barplot_price_room1.loc[barplot_price_room1.neighbourhood.isin(plot_data)]
+    sns.barplot(data=barplot_price_room.sort_values(by='price'), x='neighbourhood', y='price', palette='BrBG', ax=ax)
+    plt.xticks(rotation=90)
+    plt.xlabel('Neighbourhood', fontsize=16)
+    plt.ylabel('Price', fontsize=16)
+    plt.title('Average Price of Room', fontsize=18)
+ 
+# Toon het diagram met Streamlit
+    st.pyplot(fig)
+ 
+barplot_count_room1 = number_of_offers.reset_index()
+barplot_count_room = barplot_count_room1.loc[barplot_count_room1.neighbourhood.isin(plot_data)]
+ 
+with c4:
+    st.markdown('### Number of offers per neighbourhood')
+    fig, ax = plt.subplots(figsize=(12, 8), dpi=80)
+ 
+# Gebruik seaborn voor de staafplot
+    sns.barplot(data=barplot_count_room.sort_values(by='offers'), x='neighbourhood', y='offers', palette='BrBG', ax=ax)
+    plt.xticks(rotation=90)
+    plt.xlabel('Neighbourhood', fontsize=16)
+    plt.ylabel('Count', fontsize=16)
+    plt.title('Number of Offers', fontsize=18)
+ 
+# Toon het diagram met Streamlit
+    st.pyplot(fig)
+ 
+st.markdown('Source: https://www.kaggle.com/datasets/erikbruin/airbnb-amsterdam/')
--- pages/0_Animation_Demo.py
+++ pages/0_Animation_Demo.py
@@ -1,84 +1,176 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from typing import Any
-
-import numpy as np
-
 import streamlit as st
-from streamlit.hello.utils import show_code
-
-
-def animation_demo() -> None:
-
-    # Interactive Streamlit elements, like these sliders, return their value.
-    # This gives you an extremely simple interaction model.
-    iterations = st.sidebar.slider("Level of detail", 2, 20, 10, 1)
-    separation = st.sidebar.slider("Separation", 0.7, 2.0, 0.7885)
-
-    # Non-interactive elements return a placeholder to their location
-    # in the app. Here we're storing progress_bar to update it later.
-    progress_bar = st.sidebar.progress(0)
-
-    # These two elements will be filled in later, so we create a placeholder
-    # for them using st.empty()
-    frame_text = st.sidebar.empty()
-    image = st.empty()
-
-    m, n, s = 960, 640, 400
-    x = np.linspace(-m / s, m / s, num=m).reshape((1, m))
-    y = np.linspace(-n / s, n / s, num=n).reshape((n, 1))
-
-    for frame_num, a in enumerate(np.linspace(0.0, 4 * np.pi, 100)):
-        # Here were setting value for these two elements.
-        progress_bar.progress(frame_num)
-        frame_text.text("Frame %i/100" % (frame_num + 1))
-
-        # Performing some fractal wizardry.
-        c = separation * np.exp(1j * a)
-        Z = np.tile(x, (n, 1)) + 1j * np.tile(y, (1, m))
-        C = np.full((n, m), c)
-        M: Any = np.full((n, m), True, dtype=bool)
-        N = np.zeros((n, m))
-
-        for i in range(iterations):
-            Z[M] = Z[M] * Z[M] + C[M]
-            M[np.abs(Z) > 2] = False
-            N[M] = i
-
-        # Update the image placeholder by calling the image() function on it.
-        image.image(1.0 - (N / N.max()), use_column_width=True)
-
-    # We clear elements by calling empty on them.
-    progress_bar.empty()
-    frame_text.empty()
-
-    # Streamlit widgets automatically run the script from top to bottom. Since
-    # this button is not connected to any other logic, it just causes a plain
-    # rerun.
-    st.button("Re-run")
-
-
-st.set_page_config(page_title="Animation Demo", page_icon="📹")
-st.markdown("# Animation Demo")
-st.sidebar.header("Animation Demo")
-st.write(
-    """This app shows how you can use Streamlit to build cool animations.
-It displays an animated fractal based on the the Julia Set. Use the slider
-to tune different parameters."""
-)
-
-animation_demo()
-
-show_code(animation_demo)
+import pandas as pd
+import plost
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import math
+import plotly.graph_objects as go
+import statsmodels.api as sm
+import cbsodata
+import ipywidgets as widgets
+from ipywidgets import interact
+ 
+#inladen data
+calendar = pd.read_csv("calendar.csv")
+listing = pd.read_csv("listings.csv")
+row_to_skip = 16341
+ 
+# Read the CSV file skipping the specified row
+listingd = pd.read_csv("listings_details.csv", skiprows=lambda x: x == row_to_skip)
+neighbourhood = pd.read_csv("neighbourhoods.csv")
+ 
+#cbs api
+toc = pd.DataFrame(cbsodata.get_table_list())
+ 
+# Downloading entire dataset (can take up to 30s)
+huurverhoging = pd.DataFrame(cbsodata.get_data('83162NED'))
+ 
+#data cleaning
+df = calendar.sort_values('price', ascending = False)
+df_unique = df.drop_duplicates(subset='listing_id', keep='first').copy()
+df_unique_nan = df_unique.dropna()
+ 
+#prijs een cijfer maken
+df['price'] = df['price'].astype('str')
+df['price'] = df['price'].apply(lambda x:x.lstrip('$'))
+df['price'] = df['price'].apply(lambda x:x.replace(',',''))
+df['price'] = df['price'].astype('float')
+df_unique['price'] = df_unique['price'].astype('str')
+df_unique['price'] = df_unique['price'].apply(lambda x:x.lstrip('$'))
+df_unique['price'] = df_unique['price'].apply(lambda x:x.replace(',',''))
+df_unique['price'] = df_unique['price'].astype('float')
+listingd['price'] = listingd['price'].astype('str')
+listingd['price'] = listingd['price'].apply(lambda x:x.lstrip('$'))
+listingd['price'] = listingd['price'].apply(lambda x:x.replace(',',''))
+listingd['price'] = listingd['price'].astype('float')
+calendar['price'] = calendar['price'].astype('str').apply(lambda x:x.lstrip('$'))
+calendar['price'] = calendar['price'].apply(lambda x:x.replace(',',''))
+calendar['price'] = calendar['price'].astype('float')
+df = listing
+price = df.groupby('neighbourhood').price.mean()
+price = price.reset_index()
+number_of_offers = df.groupby('neighbourhood').count()
+number_of_offers.rename(columns={'name': 'offers'}, inplace=True)
+neighbourhood.set_index('neighbourhood', inplace=True)
+apartments = df[['latitude','longitude','room_type']]
+ 
+ 
+ 
+st.title('Analysis of Air bnb listings in Amsterdam')
+ 
+#Barplot room type
+# Bereken de telling van kamer types
+rooms = apartments['room_type'].value_counts()
+ 
+# Plot de staafdiagram in Streamlit
+fig = plt.figure(figsize=(11, 7))
+plt.bar(['Entire home/apt', 'Private room', 'Shared room'], rooms, color=['#a6611a', '#018571', 'gray'], width=0.4)
+ 
+# Toon labels en titel
+plt.xlabel('Room Type', fontsize=16)
+plt.ylabel('Count', fontsize=16)
+plt.title('Room Types in Amsterdam', fontsize = 16)
+ 
+# Toon de staafdiagram in Streamlit
+st.pyplot(fig)
+ 
+#barplot
+mean_prices = listingd.groupby('neighbourhood_cleansed')['price'].mean().sort_values().index
+ 
+# Kies een kleurenpalet voor de grafiek
+colors = sns.color_palette('Paired', n_colors=len(mean_prices))
+ 
+# Plot de staafdiagram in Streamlit
+fig, ax = plt.subplots(figsize=(10, 6))
+sns.barplot(data=df, x='neighbourhood', y='price', order=mean_prices, ax=ax, palette=colors)
+plt.title('Mean prices per neighbourhood', fontsize=16)
+ 
+# Voeg annotaties toe voor het gemiddelde aantal personen (accommodates)
+for bar, val in zip(ax.patches, listingd.groupby('neighbourhood')['review_scores_rating'].mean()):
+    ax.text(bar.get_x() + bar.get_width() / 2, bar.get_height() + 20,
+            f'{int(round(val))}', ha='center', va='bottom', fontsize=8, color='black')
+ 
+# Stel de rotatie van de x-labels in
+plt.xticks(rotation=90)
+ 
+# Toon de staafdiagram in Streamlit
+st.pyplot(fig)
+ 
+ 
+#plot accomodates
+listingd1= listingd.reset_index()
+# Creëer een figuur met matplotlib
+fig, ax = plt.subplots(figsize=(12, 8), dpi=80)
+ 
+# Gebruik seaborn voor de histogramplot in de gewenste stijl
+sns.histplot(data=listingd1, x='accommodates', ax=ax)
+ 
+plt.xticks(rotation=0)
+plt.xlabel('Accommodates', fontsize=16)
+plt.ylabel('Number of listings', fontsize=16)
+plt.title('Accommodates (number of people)', fontsize=18)
+ 
+# Toon het diagram met Streamlit
+st.pyplot(fig)
+ 
+ 
+#scatterplot
+dflist = listingd[(listingd.price <3000)&(listingd.review_scores_rating>60)&(listingd.accommodates < 7)]
+ 
+fig, ax = plt.subplots()
+scatter = sns.scatterplot(data=dflist, y='review_scores_rating', x='price', hue='accommodates')
+ 
+# Voeg labels toe aan de assen en titel
+plt.xlabel('Price')
+plt.ylabel('Review score')
+plt.title('Price vs review score')
+ 
+# Voeg legenda toe
+plt.legend(title='Guest amount')
+ 
+# Toon de scatterplot in Streamlit
+st.pyplot(fig)
+ 
+#title linear regression result
+st.title('Linear regression result')
+#linear
+st.image("Linear_result.png", caption="Linear Regression Result", use_column_width=True)
+ 
+ 
+calendar['date'] = pd.to_datetime(calendar['date'])
+calendar['year'] = calendar['date'].dt.year
+merge1 = listingd[['id', 'neighbourhood_cleansed']]
+merged = pd.merge(merge1, calendar, left_on='id', right_on='listing_id')
+ 
+neighbourhoods = ['All neighbourhoods'] + merged['neighbourhood_cleansed'].unique().tolist()
+ 
+def plot_neighbourhood(neighbourhood):
+    st.subheader(f'Mean pric per year for {neighbourhood}')
+ 
+    if neighbourhood == 'All neighbourhoods':
+        mean_prices = merged.groupby(['year'])['price'].mean().reset_index()
+    else:
+        df_neighbourhood = merged[merged['neighbourhood_cleansed'] == neighbourhood]
+        mean_prices = df_neighbourhood.groupby('year')['price'].mean().reset_index()
+ 
+    # Maak de barplot met Seaborn
+    fig, ax = plt.subplots()
+    sns.barplot(x='year', y='price', data=mean_prices, ax=ax)
+    ax.set(xlabel='Year', ylabel='Mean price')
+    # Toon de plot in Streamlit
+    st.pyplot(fig)
+ 
+# Creëer een interactieve dropdown met buurten, inclusief de optie 'Alle buurten'
+selected_neighbourhood = st.selectbox('Select neighbourhood', neighbourhoods)
+plot_neighbourhood(selected_neighbourhood)
+ 
+huurverhoging['Perioden'] = pd.to_datetime(huurverhoging.Perioden)
+huurverhoging_ams = huurverhoging.loc[(huurverhoging.RegioS == 'Amsterdam')&(huurverhoging.Perioden > "2015")]
+st.dataframe(huurverhoging_ams)
+ 
+code = '''print((calendar2019.price.mean()-calendar2018.price.mean())/calendar2018.price.mean()*100)
+                3.401724999038135'''
+st.code(code, language='python')
